dc1419d34ab88d9a0943a5ea3f4363b6f2fdfe63
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2018-10-30 01:57:28 +0200

    setup start script and heroku port

--- routes.js
+++ routes.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 app.set('view engine','ejs');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+// to run the app online with heroku us should set aport dinamic 
+const port = process.env.PORT || 3000 ;
 
 //handle static calls to assets
 app.use('/assets',express.static('assets'));
 app.use(function (req,res,next) {
 	// body...
+
 	var data = new Date().toString();
 	var log = `${data}  :: ${req.method}  ::  ${req.url}`;
 	console.log(log);
@@ -31,4 +33,5 @@ app.post('/contact',urlencodedParser,function (req,res) {
 	console.log(req.body);
   res.render('contactsucc',{data:req.body});
 });
-app.listen(3000);
+app.listen(port);
+console.log(`your on 127.0.0.1:${port}`);
